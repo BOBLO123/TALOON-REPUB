b3696db6ee3b1cf4c29b22ea3f08c8ca12811c26
LOLnuhuh <160150691+LOLnuhuh@users.noreply.github.com>
2024-03-07 13:09:31 -0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,10 @@
-
+// x is the id of the form, y is the requirements for a name.
+function check(x, y){
+  // id should have quotes around it
+  value = document.getElementById(x).value
+  if (value = (y)){
+    // put what you want it to do here
+  }
+}
+function signup(x, y){
+  //haven't done thi yet
