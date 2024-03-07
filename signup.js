ff6bd7f8df69c7024007acf56eccf78d1b73e7a1
LOLnuhuh <160150691+LOLnuhuh@users.noreply.github.com>
2024-03-07 15:37:07 -0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,10 +1,17 @@
 // x is the id of the form, y is the requirements for a name.
-function check(x, y){
+function check(x){
   // id should have quotes around it
   value = document.getElementById(x).value
-  if (value = (y)){
-    // put what you want it to do here
-  }
+  if (value > 6){
+    signup(x)
+  
+  } else {
+    alert("Password must be at least 6 characters long")
 }
-function signup(x, y){
-  //haven't done thi yet
+function signup(x){
+  //add user to sign in json
+  let signin = document.getElementById(x).value
+  let users = JSON.parse(fs.readFileSync("private/usr.json")); 
+  users.newData = signin;
+  fs.writeFileSync("private/usr.json", JSON.stringify(users));
+}
