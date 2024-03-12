fbd74d45dce7448a85cc8ddff33a87c768660fb8
LOLnuhuh <160150691+LOLnuhuh@users.noreply.github.com>
2024-03-12 10:00:48 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,3 +1,15 @@
 //Third time I've completely restarted
 
-function signup()
+if(!localStorage.getItem("credentials")) localStorage.setItem("credentials", JSON.stringify([]));
+
+var arr = JSON.parse(localStorage.getItem("credentials"));
+console.log(arr)
+
+let user_name = document.getElementById("username");
+let user_paswrd = document.getElementById("password");
+
+let store_data = () => {
+  credentials.push({username: user_name, password: user_paswrd});
+  localStorage.setItem("credentials", JSON.stringify(arr))
+
+};
