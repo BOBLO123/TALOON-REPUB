3865380d1ba6e1749bff897e41a8afa2c9266f91
LOLnuhuh <160150691+LOLnuhuh@users.noreply.github.com>
2024-03-12 09:53:45 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,28 +1,3 @@
-// get user list from usr.json
-fetch("/private/usr.json")
-//sign up. This took me 1 hour
-function signup(){
-  let username = document.getElementById("name").value
-  let pass = document.getElementById("pass").value
-  const users = JSON.parse("{'usrname':${username}, password:${pass}}")
-  if (users = "{'usrname':" + username + ", 'password':" + pass + "}"){
-    alert("Account Already Exists")
-  } else{
-    let newuser = {username: username, pass: pass}
-    JSON.stringify(newuser)
-    
-  }
-}
-//sign in
-function signin(){
-  let username = document.getElementById("name").value
-  let pass = document.getElementById("pass").value
-  const user = JSON.parse("{'usrname':" + username + ", 'password':" + pass + "}")
-  if (user = "{'usrname':" + username + ", 'password':" + pass + "}"){
-    let signedin = "true"
-    window.location.replace("talrmain.html")
-  } else{
-    alert("Account Does Not Exist")
-    window.location.replace("index.html")
-  }
-}
+//Third time I've completely restarted
+
+function signup()
