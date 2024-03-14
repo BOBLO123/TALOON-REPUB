5465c9bfa1266148741f97f577a41156220e42c9
LOLnuhuh <160150691+LOLnuhuh@users.noreply.github.com>
2024-03-14 10:50:39 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,20 +1,20 @@
 //Third time I've completely restarted
 
 function signup(){
-  let exist = localStorage.getItem("username")
-  let exist2 = localStorage.getItem("password")
+  let exist = localStorage.getItem("username-tal")
+  let exist2 = localStorage.getItem("password-tal")
   if (exist != null){
     alert("User Already Exists")
   } else if (exist2 != null){
     alert("User Already Exists")
   } else{
-  localStorage.setItem("username", document.getElementById('username'));
-  localStorage.setItem("password", document.getElementById('password'));
+  localStorage.setItem("username-tal", document.getElementById('username'));
+  localStorage.setItem("password-tal", document.getElementById('password'));
   }
 }
 function signin(){
-  localStorage.getItem("username")
-  localStorage.getItem("password")
+  localStorage.getItem("username-tal")
+  localStorage.getItem("password-tal")
   let signedin = ["true", exist, exist2]
   location.replace("talrmain.html")
 }
